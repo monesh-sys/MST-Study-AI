ce9db2a54c533bd0013fab4c63f513d962d9fd73
moneshtech <moneshsys@gmail.com>
2026-05-27 11:05:17 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,14 +1,8 @@
 from openai import OpenAI
 import os
-from openai import OpenAI
-
-api_key = os.getenv("GROQ_API_KEY")
-
-if not api_key:
-    raise Exception("GROQ_API_KEY is missing in environment variables")
 
 client = OpenAI(
-    api_key=api_key,
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY"),
     base_url="https://api.groq.com/openai/v1"
 )
 
